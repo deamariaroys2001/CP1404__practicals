e8a9584714731db402eb007ce398038d5eba95d4
Dea Roys <dea.roys@my.jcu.edu.au>
2021-03-21 20:22:50 +1000

    Add password check program

---
+++ prac_3/broken_score.py
@@ -0,0 +1,12 @@
+
+# program to determine score status
+
+score = float(input("Enter score: "))
+if 100 >= score >= 90:
+    print("Excellent")
+elif 90 > score >= 50:
+    print("Passable")
+elif 0<= score < 50:
+    print("Bad")
+else:
+    print("Invalid score")
