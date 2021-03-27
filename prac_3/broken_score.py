53baa3927c00b5074aca009a07f14e6303e27f48
Dea Roys <dea.roys@my.jcu.edu.au>
2021-03-27 22:00:26 +1000

    This function will calculate cumulative total income.

--- prac_3/broken_score.py
+++ prac_3/broken_score.py
@@ -2,16 +2,16 @@
 # program to determine score status
 
 def main():
-  score = float(input("Enter score: "))
-  print("score_status (score) ")
-   def score_status():
-       if 100 >= score >= 90:
-           return "Excellent"
-       elif 90 > score >= 50:
-           return "Passable"
-       elif 0<= score < 50:
-           return "Bad"
-       else:
-           return "Invalid score"
-   score_status()
+    score = float(input("Enter score: "))
+    print("score_status (score) ")
+
+def score_status():
+    if  score >= 90:
+        return "Excellent"
+    elif  score >= 50:
+        return "Passable"
+    elif 0<= score < 50:
+        return "Bad"
+    else:
+        return "Invalid score"
 main()
