6677c90f5ba585637796f05f670e2a82fe33b2c8
Dea Roys <dea.roys@my.jcu.edu.au>
2021-03-27 22:24:57 +1000

    This function will print the income report.

--- prac_4/total_income.py
+++ prac_4/total_income.py
@@ -13,12 +13,13 @@ def main():
         income = float(input("Enter income for month " + str(month) + ": "))
         incomes.append(income)
 
+def income_report ():
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, months + 1):
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
-
+income_report()
 
 main()
