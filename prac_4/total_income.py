20afa849e8afe3c8dfcc924cfd010d9cc45d9a98
Dea Roys <dea.roys@my.jcu.edu.au>
2021-03-29 19:42:22 +1000

    This function will read the data stored in another file.

--- prac_4/total_income.py
+++ prac_4/total_income.py
@@ -7,19 +7,19 @@ Starter code for cumulative total income program
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
-    months = int(input("How many months? "))
+    number_of_months = int(input("How many months? "))
 
-    for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+    for month in range(1, number_of_months + 1):
+        income = float(input("Enter income for month{ } : " .format(month)))
         incomes.append(income)
+    print(income_report(incomes))
 
-def income_report ():
-    print("\nIncome Report\n-------------")
-    total = 0
-    for month in range(1, months + 1):
-        income = incomes[month - 1]
+def income_report(incomes):
+
+ print("\nIncome Report\n-------------")
+total = 0
+for month, income in enumerate(incomes):
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
-income_report()
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month +1 , income, total))
 
 main()
