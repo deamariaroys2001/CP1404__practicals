d655dc741406e9738403cff394853145954318b8
Dea Roys <dea.roys@my.jcu.edu.au>
2021-04-08 19:51:00 +1000

    print invalid entry when choice not from list.

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -5,29 +5,64 @@ CP1404 Practicals week 5
 emails
 """
 
+#
+# def main():
+#     """Ask user their email address and check if it's their name."""
+#     user_email_name = {}
+#     email = input("Email:")
+#     while email != "":
+#         name = name_from_email(email)
+#         check = input("Is your name {} ? (Y/n):".format(name))
+#         if check.upper() != "Y" and check != " ":
+#             name = input("Name:")
+#         user_email_name[email] = name
+#         email = input("Email:")
+#
+#     for email, name in user_email_name.items():
+#         print("{} ({})".format(name,email))
+#
+#
+# def name_from_email(email):
+#     """get name from email address."""
+#     name_only = email.split('@')[0]
+#     parts = name_only.split('.')
+#     name = " ".join(parts).title()
+#     return name
+#
+#
+# main()
+
+# """
+# Lee Crawford
+# CP1404/CP5632 Practical
+# emails
+# """
+
+USERS = {}
+
 
 def main():
-    """Ask user their email address and check if it's their name."""
-    user_email_name = {}
-    email = input("Email:")
+    email = input("Email: ")
     while email != "":
-        name = name_from_email(email)
-        check = input("Is your name {} ? (Y/n):".format(name))
-        if check.upper() != "Y" and check != " ":
-            name = input("Name:")
-        user_email_name[email] = name
-        email = input("Email:")
+        name_and_email = email.split("@")
+        USERS[email] = return_name_from_email(name_and_email)
+        print("Is your name {}?".format(USERS[email]).title(), end="")
+        answer = input(" (Y/n) ").lower()
+        if answer == "n" or answer == "no":
+            USERS[email] = input("Name: ")
+        email = input("Email: ")
+    for key, name in USERS.items():
+        print("{} ({})".format(name, key))
 
-    for email, name in user_email_name.items():
-        print("{} ({})".format(name,email))
 
-
-def name_from_email(email):
-    """get name from email address."""
-    name_only = email.split('@')[0]
-    parts = name_only.split('.')
-    name = " ".join(parts).title()
-    return name
+def return_name_from_email(name_and_email):
+    """check if names separated by '.' then return string"""
+    if "." in name_and_email[0]:
+        names = name_and_email[0].split(".")
+        user_name = "{} {}".format(names[0], names[1])
+    else:
+        user_name = name_and_email[0]
+    return user_name
 
 
 main()
--- prac_05/hex_colors.py
+++ prac_05/hex_colors.py
@@ -15,7 +15,30 @@ while colour_name != "":
     if colour_name in COLORS_TO_CODE:
         print(COLORS_TO_CODE[colour_name])
     else:
-        print("Enter a color name the list:")
+        print("Invalid entry ")
     colour_name = input("Enter a color name from the list:")
 
 print("Thank You")
+
+# """
+# Lee Crawford
+# CP1404/CP5632 Practical
+# hex_colours
+# """
+
+# HEX_COLOURS = {"aquamarine1": "#7fffd4", "black": "#000000", "BlanchedAlmond": "#ffebcd",
+#                "BlueViolet": "#8a2be2", "burlywood3": "	#cdaa7d", "CadetBlue2": "#8ee5ee",
+#                "chartreuse2": "#76ee00", "chocolate1": "#ff7f24", "DarkGoldenrod1": "#ffb90f",
+#                "DarkOliveGreen4": "#6e8b3d"}
+#
+# empty_value = True
+#
+# colour_name = input("Enter colour name: ").lower()
+# while empty_value:
+#     colour_hash = [hex_number for key, hex_number in HEX_COLOURS.items() if colour_name in key.lower()]
+#     if not colour_hash:  # check its not empty
+#         print("Please enter a valid colour")
+#         colour_name = input("Enter colour name: ").lower()
+#     else:
+#         empty_value = False
+# print(colour_hash)
