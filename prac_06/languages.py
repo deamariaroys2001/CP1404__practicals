24bea48ab14713bf7d1438c21d76a2e167682859
Dea Roys <dea.roys@my.jcu.edu.au>
2021-04-18 13:34:55 +1000

    show a Programming Language object.

---
+++ prac_06/languages.py
@@ -0,0 +1,15 @@
+"""
+CP1404 Practical - Language.
+Dea Roys
+"""
+from prac_06.programming_language import ProgrammingLanguage
+
+def main():
+ """Represent a Programming Language object."""
+
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+
+main()
