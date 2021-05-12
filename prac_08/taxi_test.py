347099b714d619423b5335a59301b91fd59ba710
Dea Roys <dea.roys@my.jcu.edu.au>
2021-05-13 00:40:22 +1000

    tests the taxi file.

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,26 @@
+"""
+CP1404 Practical
+Taxi Test
+"""
+from prac_08.taxi import Taxi
+
+
+def main():
+    """Demo test code to show how to use taxi class."""
+    taxi = Taxi("Prius 1",100)
+    taxi.drive(40)
+    print("{}, {}, ${}".format(taxi.name,taxi.fuel, taxi.price_per_km))
+
+    taxi.start_fare()
+    taxi.drive(100)
+    print("{}, {}, ${}".format(taxi.name,taxi.fuel, taxi.price_per_km))
+
+
+main()
+
+
+
+
+
+
+
